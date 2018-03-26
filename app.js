12c8d8a110bb6a6f485863596e7fcb354f6b8931
Keegan Conway <conwaykc@knights.ucf.edu>
2018-03-26 19:00:42 -0400

    Create user routes

--- app.js
+++ app.js
@@ -1,18 +1,65 @@
 const express = require("express");
 const mongoose = require("./db/mongoose");
 const bodyParser = require("body-parser");
+const _ = require("lodash");
+
 const app = express();
+
+const User = require("./models/user");
 
 app.use(bodyParser.json());
 
-app.get('/users', function(req, res) {
-  res.send();
+app.get('/users', (req, res) => {
+  User.find({}, (err, docs)=>{
+    res.send(docs);
+  });
 });
 
-app.post('/users', function(req, res) {
-  res.send();
+app.post('/users', (req, res) => {
+  const body = _.pick(req.body, ['password', 'title', 'firstName', 'lastName']);
+
+  const user = new User(body);
+
+  user.save((err, doc) => {
+    if(err)
+      return res.send(err);
+
+    res.send(doc);
+  });
 });
 
-app.listen(3000, function() {
+app.patch('/users/:id', (req, res)=>{
+  const update = _.pick(req.body, ['password', 'title', 'firstName', 'lastName']);
+
+  User.findOneAndUpdate(
+    {
+      _id: req.params.id,
+    },
+    {
+      $set: update,
+    },
+    {
+      new: true,
+    }
+  )
+  .then((doc) => {
+    res.send(doc);
+  })
+  .catch((err) => {
+    res.send(err);
+  });
+});
+
+app.delete('/users/:id', (req, res)=>{
+  User.findOneAndRemove({_id: req.params.id})
+  .then((doc)=>{
+    res.send(doc);
+  })
+  .catch((err)=>{
+    res.send(err);
+  });
+});
+
+app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
